aeda7c712e2bf5f649708a215b8b211f6cb75f1f
josephorokpo <josephorokpo@gmail.com>
2024-04-22 13:17:39 +0000

    0-constants.js

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,20 @@
+// 0-constants.js
+
+// Modify the given JavaScript code to use const and let instead of var
+
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
+}
+
+export function getLast() {
+    return ' is okay';
+}
+
+export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+
+    return combination;
+}
+
